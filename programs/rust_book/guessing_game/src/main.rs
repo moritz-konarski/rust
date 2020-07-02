0c2a3518a27eb4b49d9ea26f54a5f18091197bfe
Moritz Konarski <konarski_m@auca.kg>
2020-07-02 14:22:59 +0600

    remove old files, reorganize, redo first programs from book

---
+++ programs/rust_book/guessing_game/src/main.rs
@@ -0,0 +1,41 @@
+use std::io;
+use std::cmp::Ordering;
+
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!\nPlease input your guess:");
+
+    let secret_number: u32 = rand::thread_rng().gen_range(1, 101);
+
+    loop {
+
+        // mutable guess variable that is a string
+        let mut guess: String = String::new();
+
+        // read a line using stdio library, takes a specified mutable reference to 
+        // a string as input
+        // returns a Result type, kind of enum, about how it went
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // convert string to int, trim before hand
+        // expect in case of failure, shadow the previous variable
+        // let guess: u32 = guess.trim().parse()
+        //    .expect("Input is not a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        // switch statement on the Result type of cmp
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too large!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
--- rust_by_example/02_comments/comments.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-
-    // this generates library documentation '///'
-    // while this is just a comment '//'
-    
-    let x = 5 * 5;
-    println!("Variable 'x' equals {}", x);
-
-}
